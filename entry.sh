4acce54c3131d7cd376512a83ddf310c952431c3
StarAppeal <info@starappeal.app>
2025-09-13 12:24:59 +0200

    add ENV_TYPE check to switch python command in entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+if [ "$ENV_TYPE" = "development" ]; then
+  echo "INFO: Läuft im Entwicklungsmodus, verwende 'poetry run'."
+  PYTHON_CMD="poetry run python3"
+else
+  echo "INFO: Läuft im Produktionsmodus, verwende 'python3'."
+  PYTHON_CMD="python3"
+fi
+
 check_internet() {
   curl --silent --head --fail --connect-timeout 5 "http://www.google.com" >/dev/null 2>&1
 }
@@ -8,15 +16,17 @@ check_internet() {
 if check_internet; then
   echo "Internet connection is available. Starting the main application..."
   cd /app
-  exec poetry run python3 led_matrix_application/main.py
+  exec $PYTHON_CMD led_matrix_application/main.py
 
 else
   echo "No internet connection. Starting WiFi Connect in setup mode..."
   cd /app
 
-  poetry run python3 led_matrix_application/run_setup_mode.py &
+  echo "starting setup mode led"
+  $PYTHON_CMD led_matrix_application/run_setup_mode.py &
 
-  /usr/local/sbin/wifi-connect --portal-passphrase "12345678" -portal-ssid "LED-Matrix-Setup" --ui-directory /app/ui || true
+  echo "starting WiFi-Connect "
+  /usr/local/sbin/wifi-connect portal-ssid "LED-Matrix-Setup" --portal-passphrase "12345678" --ui-directory /app/ui || true
 
   echo "WiFi configured! Restarting the container..."
   reboot
